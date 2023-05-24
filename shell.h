fe2d146d508c71f975888bf78a02afa8a3859710
Manalhub <manalrahali24@gmail.com>
2023-05-24 15:35:42 -0700

    add commits to header

--- shell.h
+++ shell.h
@@ -12,6 +12,7 @@
 
 extern char **environ;
 
+/* Function prototypes */
 char *_getenv(const char *envar);
 int _execute(char **args);
 int _begin(char **args);
@@ -21,22 +22,29 @@ char *read_theline(void);
 void _loop(void);
 
 #define MAX_BUFFER_SIZE 1024
+
+/* Custom getline function */
 ssize_t my_own_getline(char **lineptr, size_t *n, FILE *stream);
+
+/* Helper functions */
 ssize_t read_input(char *buffer, FILE *stream);
 int find_newline(char *buffer, int start, int end);
 void copy_to_line(char *buffer, int start, int end, char *line);
 
+/* String manipulation functions */
 char *_strtok(char *thestring, const char *delim);
 char *find_next_delim(char *str, const char *delim);
 char *find_next_token(char *str, const char *delim);
 int is_delimiter(char ch, const char *delim);
 
+/* String utility functions */
 int _putchar(char m);
 int _strcmp(const char *str1, char *str2);
 int _strlen(const char *str);
 char *_strcat(char *destr, char *sourstr);
 char *_strcpy(char *destr, char *sourstr);
 
+/* Built-in shell commands */
 int _cd(char **args);
 int _out(char **args);
 int _env(void);
