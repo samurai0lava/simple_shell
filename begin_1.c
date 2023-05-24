7718c2c7fa6fedae8561160c63aa7ea765787e63
samurai0lava <ouhsseineilyass@gmail.com>
2023-05-24 07:44:35 -0700

    i almost give up

--- begin_1.c
+++ begin_1.c
@@ -8,38 +8,38 @@
  */
 int _begin(char *args)
 {
-	pid_t pid;
-	int stat;
-	char *cmd;
+        pid_t pid;
+        int stat;
+        char *cmd;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		if (args[0][0] == '/')
-			cmd = args[0];
-		else
-			cmd = get_thepath(args[0]);
+        pid = fork();
+        if (pid == 0)
+        {
+                if (args[0] == '/')
+                        cmd = &args[0];
+                else
+                        cmd = get_thepath(args);
 
-		if (cmd)
-		{
-			execve(cmd, args, environ);
-			perror("execve");
-		}
-		else
-			perror("Command not found");
+                if (cmd)
+                {
+                        execve(cmd, &args, environ);
+                        perror("execve");
+                }
+                else
+                        perror("Command not found");
 
-		exit(1);
-	}
-	else if (pid < 0)
-	{
-		perror("fork");
-	}
-	else
-	{
-		do {
-			waitpid(pid, &stat, WUNTRACED);
-		} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
-	}
-	return (1);
+                exit(1);
+        }
+        else if (pid < 0)
+        {
+                perror("fork");
+        }
+        else
+        {
+                do {
+                        waitpid(pid, &stat, WUNTRACED);
+                } while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+        }
+        return (1);
 }
 
